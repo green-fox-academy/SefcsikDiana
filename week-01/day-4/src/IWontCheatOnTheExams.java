10e4c6e23b6e005fc783be3dacff45661dc1f33f
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-15 21:33:59 +0200

    added my first loop exercise

---
+++ week-01/day-4/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+public class IWontCheatOnTheExams {
+    // Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 100) {
+            System.out.println(a+" I won't cheat on the exam!");
+            a += 1;
+        }
+    }
+}
+
