7964f3e6c258914dc37d869982d57c5218296cfb
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-16 12:53:41 +0200

    added third exercise

---
+++ week-02/day-1/src/SumElements.java
@@ -0,0 +1,13 @@
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+public class SumElements {
+    public static void main(String[] args) {
+        //         0.  1. 2.  3.
+        // second element = 1.
+        // third element = 2.
+        int[] r = {54, 23, 66, 12};
+        int sum = r[1] + r[2] ;
+        System.out.println(sum);
+    }
+}
