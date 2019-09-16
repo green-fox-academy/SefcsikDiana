b40d3cf682f8dd3dc81e05295b080aa55b6495b3
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-16 12:25:59 +0200

    added a new exercise

---
+++ week-02/day-1/src/Third.java
@@ -0,0 +1,10 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+public class Third {
+    public static void main(String[] args) {
+        //         0. 1. 2. (tehat a 3. elem amire kivancsi vagyok, az a 2. helyen van ,mert 0tol kezdodik a sor!!)
+        int[] q = {4, 5, 6, 7};
+        System.out.println("The third number is " + q [2]);
+    }
+}
