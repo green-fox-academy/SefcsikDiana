25dbd5b85f5873193971d5fa2dd76b8337ba8b99
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-16 15:56:17 +0200

    added with 2 solutions

---
+++ week-02/day-1/src/Doubletems.java
@@ -0,0 +1,25 @@
+// - Create an array variable named `numList` with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+public class Doubletems {
+    public static void main(String[] args) {
+    int [] numList = {3, 4, 5, 6, 7};
+    // 1. megoldas, for each loop nelkul!!
+    for (int i =0; i< numList.length;i++){
+        numList [i] = numList [i] * 2;
+        // egymas ala irja ki az eredmenyt!
+        System.out.println(numList[i]);}
+        // egymas melle irja ki az eredmenyt, spaccel elvalasztva!
+        //System.out.print (numList[i] + " ");
+    }
+}
+
+/*      2. megoldas for each loop-pal:
+        int[] numList = {3, 4, 5, 6, 7};
+        for (int i = 0; i < numList.length; ++i) {
+            numList[i] = numList[i] * 2;
+        }
+        for (int i : numList) {
+            System.out.print(i + " ");
+        }
+    }
+} */
