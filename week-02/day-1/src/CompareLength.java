3ab2c9b15403a5154ed48bc1f0d33fbe13d6692a
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-16 12:41:50 +0200

    added 2. exercise

---
+++ week-02/day-1/src/CompareLength.java
@@ -0,0 +1,16 @@
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print "p2 is longer" if `p2` has more elements than `p1`
+public class CompareLength {
+    public static void main(String[] args) {
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
+        if (p1.length < p2.length) {
+            System.out.println("p2 is longer");
+        } else System.out.println("p2 is not longer than p1");
+    }
+}
+//Mindig kezd psvm!!!!
+// Mindig figyel a helyesirasra!!!
