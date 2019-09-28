9dff4a8daf6af933c0c70b732e8baeb236650bfe
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:40:37 +0200

    added Favourite number exercise

--- week-01/day-4/src/FavouriteNumber.java
+++ week-01/day-4/src/FavouriteNumber.java
@@ -1,15 +1,9 @@
 public class FavouriteNumber {
     public static void main(String[] args) {
-        int favouriteNumber = 8;
-        System.out.println("My favourite number is:" + favouriteNumber);
-
+        // 1. Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+        int favoriteNumber = 8;
+        System.out.println("My favorite number is: " + favoriteNumber + ".");
+        System.out.println();
     }
 }
-
-/* public class FavoriteNumber {
-    public static void main(String[] args) {
-        int favoriteNumber = 8;
-        // Store your favorite number in a variable (as a number)
-        // And print it like this: "My favorite number is: 8"
-    }
-} */
