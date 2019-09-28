b2100aac8509b4cc25415ffd8bc809eddf4dbf96
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:34:29 +0200

    added Count from to

--- week-01/day-4/src/CountFromtTo.java
+++ week-01/day-4/src/CountFromtTo.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 // Create a program that asks for two numbers
 // If the second number is not bigger than the first one it should print:
 // "The second number should be bigger"
-//
 // If it is bigger it should count from the first number to the second by one
 public class CountFromtTo {
     public static void main(String[] args) {
