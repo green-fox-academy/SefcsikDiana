5f0fa2b779e3e98df278bcb8dd8abf51d8dcf798
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:42:01 +0200

    added Guess the number exercise

--- week-01/day-4/src/GuessTheNumber.java
+++ week-01/day-4/src/GuessTheNumber.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
+        // Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stried number is lower
+// You found the number: 8
         int mySecretNumber = 6;
         System.out.println("Guess a number!");
         Scanner in = new Scanner(System.in);
@@ -22,12 +29,3 @@ public class GuessTheNumber {
 
     }
 }
-
-
-// Write a program that stores a number, and the user has to figure it out.
-// The user can input guesses, after each guess the program would tell one
-// of the following:
-//
-// The stored number is higher
-// The stried number is lower
-// You found the number: 8
