155f34875ad031f79d6b61232ab7342e03af3684
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:56:25 +0200

    added Print Even erxercise

--- week-01/day-4/src/PrintEven.java
+++ week-01/day-4/src/PrintEven.java
@@ -1,18 +1,10 @@
 public class PrintEven {
-    // Create a program that prints all the even numbers between 0 and 500
-    public static void main(String[] args) {
-        for (int i = 0; i <= 500; i=i+2){
-                System.out.println(i);
-        }
+  // Create a program that prints all the even numbers between 0 and 500
+  public static void main(String[] args) {
+    for (int number = 0; number <= 500 ; number++) {
+      if (number % 2 == 0){
+        System.out.println(number);
+      }
     }
+  }
 }
-
-
-/* masik, pazarlobb megoldas:
-    public static void main(String[] args) {
-        for (int i = 0; i <= 500; i++){
-            if (i % 2 == 0)
-                System.out.println(i);
-        }
-    }
-} */
