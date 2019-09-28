f89529b09aa7198038ef6e0c3ce3b94679908f31
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 19:00:15 +0200

    added Variable mutation exercise

--- week-01/day-4/src/VariableMutation.java
+++ week-01/day-4/src/VariableMutation.java
@@ -1,76 +1,77 @@
 public class VariableMutation {
-    public static void main(String[] args) {
-        int a = 3;
-        // make the "a" variable's value bigger by 10
-        a += 10;
-        System.out.println(a);
+  // osztalyneve mindig nagy betuvel kezdjuk!!!
+  public static void main(String[] args) {
 
-        int b = 100;
-        // make b smaller by 7
-        b -= 7;
-        System.out.println(b);
+    // make the "a" variable's value bigger by 10
+    int a = 3;
+    a += 10;
+    System.out.println("a = " + a);
+    System.out.println();
 
-        int c = 44;
-        // please double c's value
-        c *= 2;
-        System.out.println(c);
+    // make b smaller by 7
+    int b = 100;
+    b -= 7;
+    System.out.println("b = " + b);
+    System.out.println();
 
-        int d = 125;
-        // please divide by 5 d's value
-        d /= 5;
-        System.out.println(d);
+// please double c's value
+    int c = 44;
+    c *= c;
+    System.out.println("c = " + c);
+    System.out.println();
 
-        int e = 8;
-        // please cube of e's value
-        e *= e * e;
-        System.out.println(e);
+    // please divide by 5 d's value
+    int d = 125;
+    d /= 5;
+    System.out.println("d = " + d);
+    System.out.println();
 
-        int f1 = 123;
-        int f2 = 345;
-        // tell if f1 is bigger than f2 (print as a boolean)
-        boolean x = f1 > f2;
-        System.out.println(x);
+    // please cube of e's value
+    int e = 8;
+    e *= e;
+    System.out.println("e = " + e);
+    System.out.println();
 
-        int g1 = 350;
-        int g2 = 200;
-        // tell if the double of g2 is bigger than g1 (print as a boolean)
-        boolean y = g2 * 2 > g1;
-        System.out.println(y);
-
-        int h = 135798745;
-        // tell if it has 11 as a divisor (print as a boolean)
-        int leftOver = h % 11;
-        boolean divisor = leftOver == 0;
-        System.out.println(divisor);
-
-        int i1 = 10;
-        int i2 = 3;
-        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-        if (i1 > i2 * i2 && i1 < i2 * i2 * i2) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
-
-        int i3 = 10;
-        int i4 = 3;
-        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean) double exercises
-        boolean divisor2 = i3 > i4 * i4 && i3 < i4 * i4 * i4;
-        System.out.println(divisor2);
+    // tell if f1 is bigger than f2 (print as a boolean)
+    int f1 = 123;
+    int f2 = 345;
+    String message = (f1 > f2) ? "f1 is bigger than f2" : "f2 is bigger than f1";
+    System.out.println(message);
+  /* 2. megoldas :
+    if (f1 > f2){
+    System.out.println("f1 is bigger than f2");
+  } else System.out.println("f2 is bigger than f1"); */
 
 
-        int j1 = 1521;
-        // tell if j is dividable by 3 or 5 (print as a boolean)
-        boolean dividable = j1 % 3 == 0 ^ 5 == 0;
-        System.out.println(dividable);
+  /* 3. megoldas :
+    boolean f1IsBigger = f1 > f2;
+    System.out.println("F1 is bigger than f2: " + f1IsBigger + "\n"); */
 
-        int j = 1521;
-        // tell if j is dividable by 3 or 5 (print as a boolean)
-        if (j % 3 == 0 ^ 5 == 0) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
-    }
+    // tell if the double of g2 is bigger than g1 (print as a boolean)
+    int g1 = 350;
+    int g2 = 200;
+    boolean g2IsBigger = g2 * 2 > g1;
+    System.out.println("The double of g2 is: " + g2IsBigger + "\n");
+
+    // tell if it has 11 as a divisor (print as a boolean)
+    int h = 135798745;
+    boolean isDivisor = h % 11 == 0;
+    System.out.println("h has 11 as a divisor: " + isDivisor + "\n");
+
+    // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+    int i1 = 10;
+    int i2 = 3;
+    double i2Cube = Math.pow(i2, 3);
+    // powOf = hatvanyozas (i2 ertek, hanyadikra szeretnem emelni, itt harmadikra emelem!!!)
+    System.out.println(i2Cube);
+    boolean statementIsTrue = i1 > (i2 * i2) && i1 < (i2Cube);
+    System.out.println(statementIsTrue);
+
+    // tell if j is dividable by 3 or 5 (print as a boolean)
+    int j = 1521;
+    boolean dividable = j % 3 == 0 || j % 5 == 0;
+    System.out.println("j is dividable by 3 or 5:" + dividable);
+    // amikor azt vizsgalom, hogy oszthato vmivel
+
+  }
 }
-
