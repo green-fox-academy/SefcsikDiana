b8731122a6eec04a395acee9e9a3f5aa453a51d3
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 19:31:17 +0200

    added Print Each lines exercise

--- week-03/day-2/src/PrintEachLine.java
+++ week-03/day-2/src/PrintEachLine.java
@@ -8,6 +8,7 @@ public class PrintEachLine {
     // Write a program that opens a file called "my-file.txt", then prints each line from the file.
     // If the program is unable to read the file (for example it does not exist),
     // then it should print the following error message: "Unable to read file: my-file.txt"
+    
     try {
       Path filePath = Paths.get("my-file.txt");
       List<String> lines = Files.readAllLines(filePath);
