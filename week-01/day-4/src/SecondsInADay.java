0058a8f2268713c43535a86f2f4b39081a2b9335
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:57:58 +0200

    added 'Seconds in a Day' exercise

--- week-01/day-4/src/SecondsInADay.java
+++ week-01/day-4/src/SecondsInADay.java
@@ -1,16 +1,12 @@
-
 public class SecondsInADay {
-    public static void main(String[] args) {
-        int currentHours = 14;
-        int currentMinutes = 34;
-        int currentSeconds = 42;
+  public static void main(String[] args) {
+    // Write a program that prints the remaining seconds (as an integer) from a
+    // day if the current time is represented by the variables
+    int currentHours = 14;
+    int currentMinutes = 34;
+    int currentSeconds = 42;
+    int remainingSeconds = 24*3600-(14*3600)+(34*60)-42;
+    System.out.println("The remaining seconds from this day is " + remainingSeconds);
 
-        // Write a program that prints the remaining seconds (as an integer) from a
-        // day if the current time is represented by the variables
-
-        int DaySeconds = 86400;
-        int remainingSconds = 86400 - 14 * 60 * 60 + 34 * 60 + 42;
-        System.out.println(remainingSconds);
-
-    }
+  }
 }
