d8379d5a729799c3ea6ab334ca77e78d28304a53
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 19:06:25 +0200

    added DiceSet exercise

---
+++ src/DefineBasicInfo.java
@@ -0,0 +1,20 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+
+    //4.  Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+
+    String name = "Sefcsik Diana";
+    int age = 30;
+    double height = 1.74;
+    boolean familyStatus = true;
+
+    System.out.println("My name is " + name + ".");
+    System.out.println("My age is " + age + ".");
+    System.out.println("My height is " + height + ".");
+    System.out.println("I am married " + familyStatus + ".");
+  }
+}
