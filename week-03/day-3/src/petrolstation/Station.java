59a1d968466d3e042c235c7fd24e309762df9ca1
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 19:25:30 +0200

    added Petrol Station exercise

--- week-03/day-3/src/petrolstation/Station.java
+++ week-03/day-3/src/petrolstation/Station.java
@@ -1,4 +1,13 @@
 package petrolstation;
 
 public class Station {
+  int gasAmount;
+
+  public Station (int gasAmount){
+    this.gasAmount = gasAmount;
+  }
+  //                class tipus:( Car.java car neven fut majd)
+  public void refill(Car car){
+
+  }
 }
