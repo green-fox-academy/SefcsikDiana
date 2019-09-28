d59938ffefc8faa1f062e5bfdd14f834c924f3cf
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:48:20 +0200

    added Introduce yourself exercise

--- week-01/day-4/src/IntroduceYourself.java
+++ week-01/day-4/src/IntroduceYourself.java
@@ -1,11 +1,15 @@
 public class IntroduceYourself {
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
     public static void main(String[] args) {
-        System.out.println("Diana Sefcsik");
-        System.out.println("30");
-        System.out.println("1.72");
+    String  name = "Sefcsik Diana";
+    int age = 30;
+    double height = 1.74;
+        System.out.println("My name is " + name + ".");
+        System.out.println("My age is " + age + ".");
+        System.out.println("My height is " + height + ".");
     }
-
-
 }
-
-
