9a4d949a2a4d8139882f97d83e0f7f4c00e6d03d
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:53:09 +0200

    added Odd/Even exercise

--- week-01/day-4/src/OddEven.java
+++ week-01/day-4/src/OddEven.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class OddEven {
-    // Write a program that reads a number from the standard input,
+  // Write a program that reads a number from the standard input,
 // Then prints "Odd" if the number is odd, or "Even" if it is even.
-    public static void main(String[] args) {
-        int number;
-        System.out.println("Enter an integer to check if it is odd or even");
-        Scanner in = new Scanner(System.in);
-        number = in.nextInt();
+  public static void main(String[] args) {
+    System.out.println("Give me a number: ");
+    Scanner scanner = new Scanner(System.in);
+    int inputNumber = scanner.nextInt();
 
-        if (number % 2 == 0)
-            System.out.println("The number is even.");
-        else
-            System.out.println("The number is odd.");
+    if (inputNumber % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
     }
+  }
 }
