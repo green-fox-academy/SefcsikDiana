fc67a446d47688b42c3b9a9a0d3214fa3731a1a6
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-28 18:51:49 +0200

    added 'Milo to Km converter exercise

--- week-01/day-4/src/MileToKmConverter.java
+++ week-01/day-4/src/MileToKmConverter.java
@@ -1,29 +1,13 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class MileToKmConverter {
-    public static void main(String[] args) {
-        // Write a program that asks for a double that is a distance in miles,
-        // then it converts that value to kilometers and prints it
-        Scanner myscan = new Scanner(System.in);
-        double pi = myscan.nextDouble() ;
-
-        System.out.println(pi * 1.6 + "km");
-
-    }
-
+  public static void main(String[] args) {
+    // Write a program that asks for a double that is a distance in miles,
+    // then it converts that value to kilometers and prints it
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please give me a distance in mile: ");
+  double number = scanner.nextDouble();
+  double result = number * 0.62;
+    System.out.println("The " + number + " mile is " + result + "km.");
+  }
 }
-
-
-/*Scanner myscan = new Scanner(System.in);
-        System.out.println("what is your name?");
-        String name = myscan.nextLine();
-        System.out.println("SziaMia:" + name);
-
-
-        System.out.println("What is your age?");
-        int age = myscan.nextInt();
-        System.out.println("What??? " + age + " Just kidding!");
-
-    }
- } */
