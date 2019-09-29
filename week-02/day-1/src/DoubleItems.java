d19b97130ce058ac14d0b621cd88a4383dfc1444
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 20:01:28 +0200

    added DoubleItems exercise

--- week-02/day-1/src/DoubleItems.java
+++ week-02/day-1/src/DoubleItems.java
@@ -1,7 +1,7 @@
 public class DoubleItems {
   // - Create an array variable named `numList`
-//   with the following content: `[3, 4, 5, 6, 7]`
-// - Double all the values in the array
+  //   with the following content: `[3, 4, 5, 6, 7]`
+  // - Double all the values in the array
   public static void main(String[] args) {
     int[] numlist = {3, 4, 5, 6, 7};
     //              element != index szammal! hanem az array elemevel egyezik meg!
