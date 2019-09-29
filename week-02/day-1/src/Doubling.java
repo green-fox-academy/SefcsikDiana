65fe0c194eb250b794fba33abab8e34129effdbd
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 20:33:56 +0200

    added Doubling exercise

--- week-02/day-1/src/Doubling.java
+++ week-02/day-1/src/Doubling.java
@@ -1,17 +1,14 @@
-//- Create an integer variable named `baseNum` and assign the value `123` to it
-// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
-// - Print the result of `doubling(baseNum)`
 public class Doubling {
-    public static void main(String[] args) {
-        int baseNum = 123;
-        //Print the result of `doubling(baseNum) ezzel a kifezejessel hatarozom meg, hogy idekeruljon a kiiratas!
-       System.out.println(doubling(baseNum));
-    }
-    //called 'doubling' a funkcioba! 11. sorban, es duplazd meg az input parameteret ami, a 10.sorban parameter=(int i)!!
-    public static int doubling(int x) {
-        x *= 2;
-        //returns with an integer! 13. sor
-        return x;
-    }
-}
+  public static void main(String[] args) {
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    //- Print the result of `doubling(baseNum)`
+    int baseNum = 123;
+    int doublingBaseNum = doubling(baseNum);
+    System.out.println(doublingBaseNum);
+  }
 
+  public static int doubling(int baseNum) {
+    return baseNum * 2;
+  }
+}
