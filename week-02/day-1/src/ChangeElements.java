cf30c23aceed0cdeae2ba1ec8c9198945944c9c3
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 19:52:39 +0200

    added ChangeElements exercise

--- week-02/day-1/src/ChangeElements.java
+++ week-02/day-1/src/ChangeElements.java
@@ -1,8 +1,8 @@
 public class ChangeElements {
   // - Create an array variable named `s`
-//   with the following content: `[1, 2, 3, 8, 5, 6]`
-// - Change the 8 to 4
-// - Print the fourth element
+  //   with the following content: `[1, 2, 3, 8, 5, 6]`
+  // - Change the 8 to 4
+  // - Print the fourth element
   public static void main(String[] args) {
     int[] s = {1, 2, 3, 8, 5, 6};
     s[3] = 4;
