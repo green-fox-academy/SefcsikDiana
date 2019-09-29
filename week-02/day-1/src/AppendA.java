0b9219a1e93b906d0dcbc3d3d3e28e3b988d9386
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 20:09:50 +0200

    added AppendA exercise

--- week-02/day-1/src/AppendA.java
+++ week-02/day-1/src/AppendA.java
@@ -1,18 +1,20 @@
-//  Create the usual class wrapper
-//  and main method on your own.
-
-// - Create a string variable named `typo` and assign the value `Chinchill` to it
-// - Write a function called `appendAFunc` that gets a string as an input,
-//   appends an 'a' character to its end and returns with a string
-//
-// - Print the result of `appendAFunc(typo)`
 public class AppendA {
-    public static void main(String[] args) {
-        String typo = "Chinchill";
-        System.out.println(appendAFunc(typo));
-    }
-    public static String appendAFunc(String a){
-        String x= a + "a";
-        return x;
-    }
+  //  Create the usual class wrapper
+  //  and main method on your own.
+  // - Create a string variable named `typo` and assign the value `Chinchill` to it
+  // - Write a function called `appendAFunc` that gets a string as an input,
+  //   appends an 'a' character to its end and returns with a string
+  // - Print the result of `appendAFunc(typo)
+  public static void main(String[] args) {
+    String typo = "Chinchill";
+    String typo2 = "Kuty";
+    String typo3 = "Macsk";
+    System.out.println(appendAFunc(typo));
+    System.out.println(appendAFunc(typo2));
+    System.out.println(appendAFunc(typo3));
+  }
+  private static String appendAFunc(String word){
+    String correctWord = word + "a";
+    return correctWord;
+ }
 }
