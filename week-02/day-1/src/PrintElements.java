68b3e5b05c549e361d65242033f1393d4786d7b9
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 19:59:14 +0200

    added PrintElements exercise

--- week-02/day-1/src/PrintElements.java
+++ week-02/day-1/src/PrintElements.java
@@ -1,11 +1,12 @@
-// - Create an array variable named `numbers`
-//   with the following content: `[4, 5, 6, 7]`
-// - Print all the elements of `numbers`
 public class PrintElements {
-    public static void main(String[] args) {
-        int[] numbers = {4, 5, 6, 7};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }
+  // - Create an array variable named `numbers`
+  //   with the following content: `[4, 5, 6, 7]`
+  // - Print all the elements of `numbers`
+  public static void main(String[] args) {
+    System.out.println("The all elements of numbers array is:");
+    int[] numbers = {4, 5, 6, 7};
+    for (int i = 0; i <numbers.length ; i++) {
+      System.out.print(numbers[i] + " ");
     }
+  }
 }
