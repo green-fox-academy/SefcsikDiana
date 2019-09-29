49ea428eace7f26e95e2652b24af429d7bb9a447
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 19:57:09 +0200

    added IncrementElements exercise

--- week-02/day-1/src/IncrementElements.java
+++ week-02/day-1/src/IncrementElements.java
@@ -1,7 +1,7 @@
 public class IncrementElements {
 //  - Create an array variable named `t`
 //  with the following content: `[1, 2, 3, 4, 5]`
-//      - Increment the third element
+//  - Increment the third element
 //- Print the third element
 public static void main(String[] args) {
   int[] t = {1, 2, 3, 4, 5};
