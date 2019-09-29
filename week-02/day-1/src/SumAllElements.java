8ac77ae81f2944a3a9f387692812b75d9dd4a398
SefcsikDiana <sefcsikdiana@gmail.com>
2019-09-29 20:13:48 +0200

    added SumAllElements exercise

--- week-02/day-1/src/SumAllElements.java
+++ week-02/day-1/src/SumAllElements.java
@@ -1,20 +1,14 @@
-// - Create an array variable named `ai`
-//   with the following content: `[3, 4, 5, 6, 7]`
-// - Print the sum of the elements in `ai`
 public class SumAllElements {
-    public static void main(String[] args) {
-        int[] ai = {3, 4, 5, 6, 7};
-        int x = 0;
-
-       /* for (int i = 0; i < ai.length; i++){
-            x += ai[i];
-        }
-        System.out.println(x);
+  // - Create an array variable named `ai`
+  //   with the following content: `[3, 4, 5, 6, 7]`
+  // - Print the sum of the elements in `ai`
+  public static void main(String[] args) {
+    int [] ai = {3, 4, 5, 6, 7};
+    int sum = 0;
+    System.out.println("The sum of the elements in 'ai' array: ");
+    for (int index = 0; index < ai.length ; index++) {
+     sum += ai[index];
     }
-} */
-        for (int i : ai) {
-            x += i;
-        }
-        System.out.println(x);
-    }
+    System.out.println(sum);
+  }
 }
