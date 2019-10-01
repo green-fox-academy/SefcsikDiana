5d1cde24f02151d5653ac60480106d3946c0ae06
SefcsikDiana <sefcsikdiana@gmail.com>
2019-10-01 14:37:42 +0200

    added cloneable exercise

---
+++ week-04/day-2/src/cloneable/Person.java
@@ -0,0 +1,40 @@
+package cloneable;
+
+public class Person {
+ String name;
+ int age;
+ String gender;
+
+ public Person(String newName, int newAge, String newGender){
+   this.name = newName;
+   this.age = newAge;
+   this.gender = newGender;
+ }
+ public Person(){
+   this.name = "Jane Doe";
+   this.age = 30;
+   this.gender = "female";
+ }
+
+ public void introduce(){
+   System.out.println("HI, I'am " + this.name + ", a " + this.age + " year old " + this.gender + ".");
+ }
+ public void getGoal(){
+   System.out.println("My goal is: Live for the moment!");
+ }
+//  public static void main(String[] args) {
+//    Person johny = new Person("Johny", 32, "male");
+////    jonhy.name = "Jonhy";
+////    jonhy.age = 32;
+////    jonhy.gender = "male";
+//    johny.introduce();
+//    johny.getGoal();
+//
+//    System.out.println();
+//
+//    Person defaultPerson = new Person();
+//    defaultPerson.introduce();
+//    defaultPerson.getGoal();
+//    }
+  }
+
